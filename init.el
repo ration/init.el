bfc73a897bb32015e4e6489a6f0d6106cc8d2c25
Tatu Lahtela <lahtela@iki.fi>
2020-06-03 16:03:25 +0300

    Fix broken ORG. Still should change to load MELPA in init.el

--- init.el
+++ init.el
@@ -1,13 +1,18 @@
+(setq load-path (remove "/usr/share/emacs/26.3/lisp/org" load-path)) 
+(add-to-list 'load-path "~/.emacs.d/elpa/org-9.3.6")
+
 (setq vc-follow-symlinks nil)
 
 (if (version< emacs-version "27.0")
     (load-file (expand-file-name "early-init.el" user-emacs-directory))
 )
 
-;; Load the rest from org file
-(require 'org)
+
+(message (format "%s" load-path))
+(setq load-prefer-newer t)
 (setq org-src-tab-acts-natively t)
+(require 'org)
 (org-babel-load-file
- (expand-file-name "settings.org"
-                   user-emacs-directory))
+(expand-file-name "settings.org"
+                  user-emacs-directory))
 
