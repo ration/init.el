5d5436fb51eb4d395d79170729f6a97d3779bb76
Tatu Lahtela <lahtela@iki.fi>
2020-06-26 12:30:33 +0300

    Move melpa out of init. Other updates

--- init.el
+++ init.el
@@ -1,6 +1,20 @@
-(setq load-path (remove "/usr/share/emacs/26.3/lisp/org" load-path)) 
-(add-to-list 'load-path "~/.emacs.d/elpa/org-9.3.6")
+;; TODO move elpa init here
+(require 'package)
+(setq package-enable-startup nil)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
+
+;(setq load-path (remove "/usr/share/emacs/26.3/lisp/org" load-path))
+;(add-to-list 'load-path "~/.emacs.d/elpa/org-9.3.6")
+
+;; Dont prompt about vc symlinks, just goto
 (setq vc-follow-symlinks nil)
 
 (if (version< emacs-version "27.0")
@@ -10,9 +24,13 @@
 
 (message (format "%s" load-path))
 (setq load-prefer-newer t)
+;; Some settings before even entering the actual settings file
+;; Native indentation of source blocks without indentation
+(setq org-edit-src-content-indentation 0)
 (setq org-src-tab-acts-natively t)
 (require 'org)
 (org-babel-load-file
 (expand-file-name "settings.org"
                   user-emacs-directory))
 
+(put 'upcase-region 'disabled nil)
