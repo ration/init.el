77daac5841e360ce30f7eaf8aa87e7243eb1b2c8
Tatu Lahtela <lahtela@iki.fi>
2021-03-13 09:45:35 +0200

    Update configuration to nativecomp and straight.el

--- init.el
+++ init.el
@@ -1,36 +1,51 @@
-;; TODO move elpa init here
-(require 'package)
-(setq package-enable-startup nil)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(require 'use-package-ensure)
-(setq use-package-always-ensure t)
-
-;(setq load-path (remove "/usr/share/emacs/26.3/lisp/org" load-path))
-;(add-to-list 'load-path "~/.emacs.d/elpa/org-9.3.6")
+(setq user-init-file (or load-file-name (buffer-file-name)))
+(setq user-emacs-directory (file-name-directory user-init-file))
 
 ;; Dont prompt about vc symlinks, just goto
 (setq vc-follow-symlinks nil)
 
-(if (version<= "27" emacs-version)
-    (load-file (expand-file-name "early-init.el" user-emacs-directory))
-)
+(load-file (expand-file-name "early-init.el" user-emacs-directory))
 
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+(straight-use-package 'el-patch)
+(setq straight-use-package-by-default t)
 
 (message (format "%s" load-path))
 (setq load-prefer-newer t)
-;; Some settings before even entering the actual settings file
-;; Native indentation of source blocks without indentation
+;;;; Some settings before even entering the actual settings file
+;;;; Native indentation of source blocks without indentation
 (setq org-edit-src-content-indentation 0)
 (setq org-src-tab-acts-natively t)
 (require 'org)
 (org-babel-load-file
-(expand-file-name "settings.org"
-                  user-emacs-directory))
+ (expand-file-name "settings.org"
+                   user-emacs-directory))
 
 (put 'upcase-region 'disabled nil)
+(put 'erase-buffer 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("02591317120fb1d02f8eb4ad48831823a7926113fa9ecfb5a59742420de206e0" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
