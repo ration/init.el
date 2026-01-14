9d503a0a4f526bd7b57e2db53e64563518c192e9
Tatu Lahtela <tatu.lahtela@paretosoftware.fi>
2026-01-14 10:27:01 +0200

    early init

--- early-init.el
+++ early-init.el
@@ -1,18 +1,19 @@
-;; (setq debug-on-error t)
-(scroll-bar-mode -1)
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(show-paren-mode 1)
-(setq vc-follow-symlinks t)
-(if (find-font (font-spec :name "FiraMono Nerd"))
-    (set-face-attribute 'default nil
-                    :family "FiraMono Nerd"
-                    :height 100
-                    :weight 'normal
-                    :width 'normal)
-)
+;;; Hide 
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
 
-(setq inhibit-splash-screen t)
 
-;; emacs-ng setting for straight
-;; (setq ng-straight-bootstrap-at-startup t)
+;; Prevent built-in package.el from loading
+(setq package-enable-at-startup nil
+      package-archives nil)
+
+
+(setq frame-inhibit-implied-resize nil) 
+(setq frame-resize-pixelwise t)
+(setq frame-inhibit-implied-resize t)
+(setq redisplay-skip-fontification-on-input t)
+(setq fast-but-imprecise-formatting t)
+(setq x-gtk-use-system-tooltips nil)
+
+(setq gc-cons-threshold 100000000) ;; 100MB
